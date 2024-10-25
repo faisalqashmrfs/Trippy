d237ba974d4c52f3e1d4491c7370049162286b65
faisalqashmrfs <125458443+faisalqashmrfs@users.noreply.github.com>
2024-10-25 22:41:47 +0300

    update2

--- src/Components/RecentCard/RecentCard.jsx
+++ src/Components/RecentCard/RecentCard.jsx
@@ -4,7 +4,7 @@ export default function RecentCard({ title, des, Photo1 }) {
   return (
     <div className='RecentCard'>
       <div className='imgBorder'>
-        <img src={Photo1} alt="" />
+        <img src={Photo1} alt="Image" />
       </div>
       <h3>{title}</h3>
       <p>{des}</p>
